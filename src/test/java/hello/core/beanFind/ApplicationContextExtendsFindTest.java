461fd00904efea4abb3fd168232ce6fd4afac684
Link <mexlove2@gmail.com>
2023-07-28 02:22:35 +0900

    관련 스프링빈 전체 조회

--- src/test/java/hello/core/beanFind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanFind/ApplicationContextExtendsFindTest.java
@@ -2,7 +2,7 @@ package hello.core.beanFind;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountpolicy;
+import hello.core.discount.RateDiscountPolicy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
@@ -30,14 +30,14 @@ public class ApplicationContextExtendsFindTest {
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면 빈 이름을 지정하면 된다")
     void findByParentTypeBeanName(){
         DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
-        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountpolicy.class);
+        assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
     }
 
     @Test
     @DisplayName("특정 하위 타입으로 조회")
     void findBeanBysubtype(){
-        RateDiscountpolicy bean = ac.getBean(RateDiscountpolicy.class);
-        assertThat(bean).isInstanceOf(RateDiscountpolicy.class);
+        RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
+        assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
     }
 
     @Test
@@ -63,7 +63,7 @@ public class ApplicationContextExtendsFindTest {
     static class TestConfig{
         @Bean
         public DiscountPolicy rateDiscountPolicy(){
-            return new RateDiscountpolicy();
+            return new RateDiscountPolicy();
         }
 
         @Bean
